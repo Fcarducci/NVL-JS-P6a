105e35c5524ebbc4eef6bbc4645d05b5ab6df61e
Fcarducci <fcarducci20@gmail.com>
2020-02-17 22:45:00 +0100

    variables

---
+++ Variables.js
@@ -0,0 +1,5 @@
+var name = "Francesco"
+var lastName = "Carducci"
+var days = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
+const gravity = 9.14
+var oddNumber = [1, 3, 5]
